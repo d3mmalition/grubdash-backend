00a2b9265c96894bc8d45437241ae71fffdc50bd
d3mmalition <ecarr.gardner@gmail.com>
2023-01-04 07:30:36 -0500

    creating dish data

--- src/dishes/dishes.router.js
+++ src/dishes/dishes.router.js
@@ -1,5 +1,14 @@
 const router = require("express").Router();
 
 // TODO: Implement the /dishes routes needed to make the tests pass
+router.route("/")
+.get(controller.list)
+.post(controller.create)
+.all(methodNotAllowed)
+
+router.route("/:dishId")
+.get(controller.read)
+.put(controller.update)
+.all(methodNotAllowed)
 
 module.exports = router;
